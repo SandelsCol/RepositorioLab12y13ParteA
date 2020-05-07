fedaad88e8c6ae8a0500f26e4f1c5e60e23654f2
SandelsCol <62896050+SandelsCol@users.noreply.github.com>
2020-05-06 19:33:39 -0500

    #Creación de la función generador/Relación de cartas con generador

--- Lab12y13_SantiagoDelValle_jhonCorzo.py
+++ Lab12y13_SantiagoDelValle_jhonCorzo.py
@@ -9,17 +9,17 @@ def imprimir(x,y):
     print("Estas son las cartas estandares",cartas,"y a continuación las premium",premium)
 imprimir(Conteo_final_Normal,Conteo_final_premium)
 
-#Creación de la función generador
+#Creación de la función generador/Relación de cartas con generador
 def generador(n,p):
-    if 1<=n<=10 and 1<=p<=10:
-        Lista_A=[1,2,3,4,5,6,7,8,9,10]
-        Lista_r=Lista_A[p:n]
-        print(Lista_r)
+    if 0<=n<=50 and 50<=p<=100:
+        cartas = ["Payne" , "Hendrix" , "Stone", "Coffey" , "Whitaker" , "Pope" , "Bleach" , "Arc" , "Fleming" , "Hardin" , "Robiar" , "Mccullough" , "Mooney" , "Reynolds" , "Short" , "Stanton" , "Deadman" , "Stonehammer" , "Smith" , "Patrick", "Crane" , "Cargane" , "Powers" , "Wade" , "Joseph" , "Savage" , "Houston", "Merritt" , "Nuke" , "Barnett" , "Acosta" , "Duke" , "Sellers" , "Blake", "Schneider" , "Stone" , "Cannon" , "Garrison" , "Randall" , "Leon" , "Buck" , "Shannon" , "Delaney" , "Mckinney" , "Dodademocles" , "Flowers" , "Whitehead" , "Kirby" , "Park" , "Shannon" , "Vlad" , "Pepin" , "Mcguire" , "Murray" , "Rush" , "Aramis" , "Fletcher" , "Mcfadden" , "Deleon" , "Luke" , "Lindsay" , "Payne" , "Gerbvo" , "Hubbard" , "Burnett" , "Bryan" , "Ratliff" , "Carlson" , "Parsons" , "Deadmeat" , "Crimson", "Wilson" , "Terry" , "Hancock" , "Hightower" , "Burns" , "Austin" , "Nightwalker" , "Thetis" , "Owen" , "Tate" , "Simmons" , "Grant" , "Barber" , "Talos" , "Ashes" , "Alston" , "Clayton" , "Mcbride" , "Huffman" , "Lightbringer" , "Blankenship" , "Higgins" , "Saint" , "Graham" , "Hodor" , "Ellison" , "Roberts" , "Odom" , "Mann"]
+        jugador=cartas[n-1:p]
+        print("La nueva baraja llamada jugador contiene las siguientes cartas",jugador,"Siendo un total de",len(jugador),"cartas")
     else:
-        print("El sistema solo acepta numeros del 1 al 10")
-n=int(input("""Digite Una cantidad de N elementos,Siendo 1 su minimo y 10 su máximo
+        print("El sistema no acepta esos números , siga por favor los procedimientos")
+n=int(input("""Digite Una cantidad de N elementos,Siendo 1 su minimo y 50 su máximo
 """))
-p=int(input("""Digite Una cantidad de N elementos,Siendo 1 su minimo y 10 su máximo
+p=int(input("""Digite Una cantidad de N elementos,Siendo 50 su minimo y 100 su máximo
 """))
 generador(n,p)
 print(generador)
